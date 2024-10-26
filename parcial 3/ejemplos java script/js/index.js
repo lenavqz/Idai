df419bf3b440bced64efcb5b831301fb8abffcc1
lenavqz <al45519@un.edu.mx>
2024-10-26 00:54:41 -0600

    index
    cambios

---
+++ parcial 3/ejemplos java script/js/index.js
@@ -0,0 +1,6 @@
+console.log("Hola mundo, desde mi archivo ");
+alert("Esto es un mensaje para el mundo");
+console.log(5+360);
+console.warn("Estamos dando tips a los alumnos ");
+console.info(" Esto es importante");
+console.error("Psicología");
