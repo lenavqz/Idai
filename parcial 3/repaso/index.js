99391b7fe135883d70b3089daabf58f58a2aa420
lenavqz <al45519@un.edu.mx>
2024-10-29 09:12:39 -0600

    cambios
    modificaciones

---
+++ parcial 3/repaso/index.js
@@ -0,0 +1,9 @@
+// alerta en js
+//alert("Prometeo");
+//alert( "alien");
+console.log ("Hola, saludamos a la consola");
+//console.log(2+2);
+//console.info("Mostrando una informacion");
+//console.warn("estamos mostrando una advertencia ");
+//console.error("Mostrando un error");
+//document.write("Hola este mensaje se mostrara en mi web")
