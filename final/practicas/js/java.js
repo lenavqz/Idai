1ccabd5d07ed1df99049fa9d3d253c693aa74358
lenavqz <al45519@un.edu.mx>
2024-11-22 09:00:55 -0600

    cambios
    cambios

--- final/practicas/js/java.js
+++ final/practicas/js/java.js
@@ -1,38 +1,63 @@
-var btnMenuopen = document.getElementById("btnMenuOpen"),
+var btnMenuOpen = document.getElementById("btnMenuOpen"),
     btnMenuClose = document.getElementById("btnMenuClose"),
-    menuResposive = document.getElementById("menuBar");
-    
+    menuResponsive = document.getElementById("menuBar");
 
+btnMenuOpen.addEventListener("click", function(){
+    menuResponsive.classList.add("active");
+});
 
-    btnMenuopen.addEventListener("click", function (){
-        menuResponsive.classList.add("active");
-    });
-
-
-    btnMenuClose.addEventListener("click", function (){
-        menuResposive.classList.remove("active");
-    });
-
+btnMenuClose.addEventListener("click", function(){
+    menuResponsive.classList.remove("active");
+});
 
 var enlaces = document.getElementById("enlaces");
 
- enlaces.addEventListener("click", function(){
-    menuResposive.style.transformStyle = "0.5s"
-    menuResposive.classList.remove("active");
+enlaces.addEventListener("click", function(){
+    menuResponsive.style.transformStyle = "0.5s"
+    menuResponsive.classList.remove("active");
 });
-  
+
+// SLIDER DE PRODUCTOS
+var  contenedor = document.querySelector('.slider'),
+     btnIzquierdo = document.getElementById("btn-izquierda"),
+     btnDerecho = document.getElementById("btn-derecha");
+
+     // EVENTO PARA EL BOTON DERECHO
+     btnDerecho.addEventListener("click", function () {
+          contenedor.scrollLeft += contenedor.offsetWidth;  
+     });
+
+     // EVENTO PARA EL BOTON IZQUIERDO
+     btnIzquierdo.addEventListener("click", function () {
+          contenedor.scrollLeft -= contenedor.offsetWidth;  
+     });
 
 
-var contenedor = document.querySelector('.slider'),
-    btnIzquierdo = document.getElementById("btn-izquierda"),
-    btnDerecho = document.getElementById("btn-derecha");
-    
-    btnDerecho.addEventListener("click", function (){
-        contenedor.scrollLeft += contenedor.offseWidth;
+var formulario = document.getElementById("formulario")
 
-    });
+function validar(e){
+    var inputNombre = document.getElementById("nombre")
+    var inputEmail = document.getElementById("email")
+    var inputComments = document.getElementById("comentarios")
 
-    btnIzquierdo.addEventListener("click", function (){
-        contenedor.scrollLeft -= contenedor.offseWidth;
-    });
+    if (inputNombre.value == 0 
+        || inputEmail.value == 0
+            ||inputComments.value == 0){
+                e.preventDefault();
+                alert("Te faltan todos los campos");
+            }
+
+            else
+            {
+                e.preventDefault();
+                alert("Datos enviados"); 
+
+                inputNombre.value = "";
+                inputEmail.value = "";
+                inputComments.value = "";
+            }
+        }
+
+
+formulario.addEventListener("submit", validar);
 
